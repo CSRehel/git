31968e62325f34d383c55163060e6fd5bbb38e60
Cinthya Silva <cinthya.rizz98@gmail.com>
2022-07-14 23:42:41 -0400

    primer commit para usar comando amend. Comando usado.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 alert('Este archivo sera borrado para probar el comando reset!')
 
-//borre el archivo de la papelera, e hice el reset hard. Todo esta recuperado.
+// borre el archivo de la papelera, e hice el reset hard. Todo esta recuperado.
+// esta linea esta siendo agregada para usar el comando amend
 
 const a = 1;
 const b = 2;
+const c = 3;
 
-const suma = (a + b)
+const suma = (a + b + c)
 console.log(suma);
